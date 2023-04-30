d37a9f22a9ed5e8d04d7145448b57f4d310b562d
DrDRR <78471758+DrDRR@users.noreply.github.com>
2023-04-30 18:15:10 +0800

    Rewrite partial codes with the help of GPT

--- renpy-unapk.py
+++ renpy-unapk.py
@@ -3,65 +3,148 @@ import zipfile
 import os
 import shutil
 import argparse
-from unrpyc import *
+import glob
+import itertools
+from os import path, walk
+from operator import itemgetter
+
+import unrpyc
+from decompiler import magic
 
 
-def remove_x (dir2 : str, some_text : str) -> None:
-    "递归重命名"
-    names = os.listdir(dir2)
+def remove_x(dir_path: str, some_text: str) -> None:
+    """Recursively remove specified text from all file and directory names in the given directory."""
+    
+    try:
+        names = os.listdir(dir_path)
+    except OSError as e:
+        print(f"Error: {e}")
+        return
 
     for name in names:
-        sub_path = os.path.join(dir2, name)
+        sub_path = os.path.join(dir_path, name)
 
         if os.path.isdir(sub_path):
             remove_x(sub_path, some_text)
 
-        name = name.replace(some_text, '')
-        new_path = os.path.join(dir2, name)
+        name_parts = os.path.splitext(name)
+        name = name_parts[0].replace(some_text, '') + name_parts[1]
+        new_path = os.path.join(dir_path, name)
 
-        os.rename(sub_path, new_path)
+        try:
+            os.rename(sub_path, new_path)
+        except OSError as e:
+            print(f"Error renaming {sub_path} to {new_path}: {e}")
 
+def extract(apkfile: str) -> None:
+    """Extracts required files from an APK archive and moves them to a specific folder."""
+    print(f"正在还原{apkfile}……")
 
-    # unrpycfile = open(os.path.join(dir3, 'un.rpyc'), 'wb')
-    # unrpycfile.write(unrpyccode)
-    # unrpycfile.close()
+    # Get the base name of the APK file
+    apk_base = os.path.basename(apkfile)
 
-def extract(apkfile : str) -> None:
-    "解压apk"
-    print(f"{i}正在还原……")
+    # Create required directories
+    game_folder = os.path.join(os.getcwd(), apk_base[:-4])
+    temp_folder = os.path.join(os.getcwd(), "TMP_FOR_UNAPK")
+    try:
+        os.makedirs(game_folder, exist_ok=True)
+        os.makedirs(temp_folder, exist_ok=True)
+    except OSError as e:
+        print(f"Error creating directory: {e}")
+        return
+
+    # Extract files to the temporary folder
     with zipfile.ZipFile(apkfile) as zf:
-        game_folder = os.path.join(os.getcwd(), apkfile[:-4])
-        temp_folder = os.path.join(os.getcwd(), "TMP_FOR_UNAPK")
-        try:
-            os.makedirs(game_folder)
-        except FileExistsError:
-            pass
-        try:
-            os.makedirs(temp_folder)
-        except FileExistsError:
-            pass
-
         zf.extractall(temp_folder)
 
-        shutil.move(os.path.join(temp_folder, 'assets', 'x-game'), game_folder)
-        shutil.move(os.path.join(temp_folder, 'res', 'mipmap-xxxhdpi-v4', 'icon_background.png'), os.path.join(game_folder, 'android-icon_background.png'))
-        shutil.move(os.path.join(temp_folder, 'res', 'mipmap-xxxhdpi-v4', 'icon_foreground.png'), os.path.join(game_folder, 'android-icon_foreground.png'))
-        shutil.move(os.path.join(temp_folder, 'assets', 'android-presplash.jpg'), game_folder)
+    # Move required files to the game folder
+    shutil.move(os.path.join(temp_folder, 'assets', 'x-game'), game_folder)
+    shutil.move(os.path.join(temp_folder, 'res', 'mipmap-xxxhdpi-v4', 'icon_background.png'), os.path.join(game_folder, 'android-icon_background.png'))
+    shutil.move(os.path.join(temp_folder, 'res', 'mipmap-xxxhdpi-v4', 'icon_foreground.png'), os.path.join(game_folder, 'android-icon_foreground.png'))
+    shutil.move(os.path.join(temp_folder, 'assets', 'android-presplash.jpg'), game_folder)
 
-        tempfiles = os.listdir(temp_folder)
-        for j in tempfiles:
-            j_path = os.path.join(temp_folder, j)
-            try:
-                os.remove(j_path)
-            except:
-                shutil.rmtree(j_path)
-        shutil.rmtree(temp_folder)
+    # Remove temporary files and directories
+    tempfiles = os.listdir(temp_folder)
+    for j in tempfiles:
+        j_path = os.path.join(temp_folder, j)
+        try:
+            os.remove(j_path)
+        except:
+            shutil.rmtree(j_path)
+    shutil.rmtree(temp_folder)
 
-        remove_x(game_folder, 'x-')
-        print(f"{i}还原完毕！")
+    # Remove 'x-' from directory and file names in the game folder
+    remove_x(game_folder, 'x-')
+    
+    print(f"{apk_base} 已成功提取至 {game_folder}")
 
-def unrpyc(apkfile : str) -> None:
+def unrpyc_main(apkfile : str, args) -> None:
     "Code from unrpyc.main"
+    print(f"正在反编译{apkfile}...")
+
+    if args.write_translation_file and not args.clobber and path.exists(args.write_translation_file):
+        pass
+
+    if args.translation_file:
+        with open(args.translation_file, 'rb') as in_file:
+            args.translations = in_file.read()
+
+    # Expand wildcards
+    def glob_or_complain(s):
+        retval = glob.glob(s)
+        if not retval:
+            print("File not found: " + s)
+        return retval
+    filesAndDirs = list(map(glob_or_complain, [apkfile[:-4]]))
+    # Concatenate lists
+    filesAndDirs = list(itertools.chain(*filesAndDirs))
+
+    # Recursively add .rpyc files from any directories passed
+    files = []
+    for k in filesAndDirs:
+        if path.isdir(k):
+            for dirpath, _, filenames in walk(k):
+                files.extend(path.join(dirpath, j) for j in filenames if len(j) >= 5 and j.endswith(('.rpyc', '.rpymc')))
+        else:
+            files.append(k)
+    
+    # Check if we actually have files. Don't worry about
+    # no parameters passed, since ArgumentParser catches that
+    if len(files) == 0:
+        print("No script files to decompile.")
+        return
+
+    files = [(args, x, path.getsize(x)) for x in files]
+
+    # Decompile in the order Ren'Py loads in
+    files.sort(key=itemgetter(1))
+    results = list(map(unrpyc.worker, files))
+
+    if args.write_translation_file:
+        print("Writing translations to %s..." % args.write_translation_file)
+        translated_dialogue = {}
+        translated_strings = {}
+        good = 0
+        bad = 0
+        for result in results:
+            if not result:
+                bad += 1
+                continue
+            good += 1
+            translated_dialogue.update(magic.loads(result[0], unrpyc.cls_factory_75))
+            translated_strings.update(result[1])
+        with open(args.write_translation_file, 'wb') as out_file:
+            magic.safe_dump((args.language, translated_dialogue, translated_strings), out_file)
+
+    else:
+        # Check per file if everything went well and report back
+        good = results.count(True)
+        bad = results.count(False)
+
+    print("RPYC反编译完成！")
+
+def parse_arg():
+    "Parse args."
     parser = argparse.ArgumentParser(description="Decompile .rpyc/.rpymc files")
 
     parser.add_argument('-c', '--clobber', dest='clobber', action='store_false',
@@ -106,110 +189,34 @@ def unrpyc(apkfile : str) -> None:
                         help="Tries some workarounds against common obfuscation methods. This is a lot slower.")
 
     args = parser.parse_args()
+    return args
 
-    if args.write_translation_file and not args.clobber and path.exists(args.write_translation_file):
-        pass
 
-    if args.translation_file:
-        with open(args.translation_file, 'rb') as in_file:
-            args.translations = in_file.read()
+def main():
+    "Main."
+    args = parse_arg()
 
-    # Expand wildcards
-    def glob_or_complain(s):
-        retval = glob.glob(s)
-        if not retval:
-            print("File not found: " + s)
-        return retval
-    filesAndDirs = list(map(glob_or_complain, [apkfile[:-4]]))
-    # Concatenate lists
-    filesAndDirs = list(itertools.chain(*filesAndDirs))
+    print("RenPy-UnApk：将APK格式的RenPy游戏恢复为工程文件")
+    print("By Koshiro, Version 1.3")
+    print("\n使用前请将apk文件放在本目录下\n")
 
-    # Recursively add .rpyc files from any directories passed
-    files = []
-    for k in filesAndDirs:
-        if path.isdir(k):
-            for dirpath, _, filenames in walk(k):
-                files.extend(path.join(dirpath, j) for j in filenames if len(j) >= 5 and j.endswith(('.rpyc', '.rpymc')))
-        else:
-            files.append(k)
+    allfiles = os.listdir()
+    apkfile = ''
+    for i in allfiles:
+        if i.lower().endswith(".apk"):
+            apkfile = i
+            try:
+                extract(apkfile)
+                unrpyc_main(apkfile, args)
+                print(f"{apkfile}还原完成！")
+            except Exception as e:
+                print(f"{apkfile} 还原失败。错误信息：\n{e}")
+
+    if not apkfile:
+        print("没有找到apk文件。你确定文件在此目录下吗？")
     
-    # Check if we actually have files. Don't worry about
-    # no parameters passed, since ArgumentParser catches that
-    if len(files) == 0:
-        print("No script files to decompile.")
-        return
+    input('\n按回车键退出')
 
-    files = [(args, x, path.getsize(x)) for x in files]
 
-    # Decompile in the order Ren'Py loads in
-    files.sort(key=itemgetter(1))
-    results = list(map(worker, files))
-
-    if args.write_translation_file:
-        print("Writing translations to %s..." % args.write_translation_file)
-        translated_dialogue = {}
-        translated_strings = {}
-        good = 0
-        bad = 0
-        for result in results:
-            if not result:
-                bad += 1
-                continue
-            good += 1
-            translated_dialogue.update(magic.loads(result[0], cls_factory_75))
-            translated_strings.update(result[1])
-        with open(args.write_translation_file, 'wb') as out_file:
-            magic.safe_dump((args.language, translated_dialogue, translated_strings), out_file)
-
-    else:
-        # Check per file if everything went well and report back
-        good = results.count(True)
-        bad = results.count(False)
-
-    print("RPYC反编译完成！")
-
-print("RenPy-UnApk：将APK格式的RenPy游戏恢复为工程文件")
-print("By Koshiro, Version 1.3")
-print("\n使用前请将apk文件放在本目录下\n")
-
-allfiles = os.listdir()
-apkfile = ''
-for i in allfiles:
-    if i.lower().endswith(".apk"):
-        apkfile = i
-        try:
-            extract(apkfile)
-            unrpyc(apkfile)
-            print(f"{apkfile}还原完成！")
-        except Exception as e:
-            print(f"{apkfile} 还原失败。错误信息：\n{e}")
-
-if not apkfile:
-    print("没有找到apk文件。你确定文件在此目录下吗？")
-# apkfile = i
-    
-# with zipfile.ZipFile(apkfile) as zf:
-#     zf.extractall(apkfile.split(".")[0])
-
-# gamefolder = os.path.join(os.getcwd(), apkfile.strip(".apk"))
-# os.makedirs(gamefolder)
-
-# shutil.move(os.path.join(os.getcwd(), 'assets', 'x-game'), gamefolder)
-# shutil.move(os.path.join(os.getcwd(), 'res', 'mipmap-xxxhdpi-v4', 'icon_background.png'), os.path.join(gamefolder, 'android-icon_background.png'))
-# shutil.move(os.path.join(os.getcwd(), 'res', 'mipmap-xxxhdpi-v4', 'icon_foreground.png'), os.path.join(gamefolder, 'android-icon_foreground.png'))
-# shutil.move(os.path.join(os.getcwd(), 'assets', 'android-presplash.jpg'), gamefolder)
-
-# tempfiles = os.listdir()
-# for i in tempfiles:
-#     if i == apkfile.strip(".apk") or i.endswith('.py') or i.endswith('.apk') or i.endswith('.exe'):
-#         pass
-#     elif not i in allfiles:
-#         try:
-#             os.remove(i)
-#         except:
-#             shutil.rmtree(i)
-
-# remove_x(gamefolder, 'x-')
-#writeunrpyc(os.path.join(gamefolder, 'game'))
-
-input('\n按回车键退出')
+if __name__=='__main__':
+    main()
